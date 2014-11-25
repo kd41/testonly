7204bd80294479847359ded03ec84b77f9cfbc91
kd41 <alekseim@Playtech-A1104.EE.playtech.corp>
2014-11-25 09:49:44 +0200

    test name

--- src/ee/test/thread/CancelThread.java
+++ src/ee/test/thread/CancelThread.java
@@ -19,7 +19,7 @@ public class CancelThread {
         MyCallable myCallable = new MyCallable();
         Future<String> futureResult = service.submit(myCallable);
         String result = null;
-        try {
+        try { 
             result = futureResult.get(1000, TimeUnit.MILLISECONDS);
         } catch (TimeoutException e) {
             System.out.println("No response after one second");
